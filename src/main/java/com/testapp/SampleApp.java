c3aae83bc4e2cd85426214299339ebb079016984
Vishal <vishalsharma24295@gmail.com>
2023-02-07 15:42:16 +0530

    first

---
+++ src/main/java/com/testapp/SampleApp.java
@@ -0,0 +1,13 @@
+package com.testapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class SampleApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
